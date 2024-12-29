c853d740e937fdb3ad4e07defffc8b31919ea24d
soyello <wodus9193@naver.com>
2024-12-30 00:35:04 +0900

    ProductInfo 컴포넌트 생성

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+interface AvatarProps {
+  src: string | null;
+}
+
+const Avatar = ({ src }: AvatarProps) => {
+  return (
+    <Image
+      className='w-10 h-10 rounded-full'
+      height={30}
+      width={30}
+      alt='Avatar'
+      src={src || 'https://via.placeholder.com/400x400?text=no+user+image'}
+      priority={true}
+    />
+  );
+};
+export default Avatar;
