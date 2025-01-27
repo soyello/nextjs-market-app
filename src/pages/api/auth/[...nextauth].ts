e73696c4a7e4caab256343bf7efa85a0a44c57db
soyello <wodus9193@naver.com>
2025-01-27 20:56:46 +0900

    1. upload 페이지 2. ImageUpload 컴포넌트 생성하기

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -53,8 +53,6 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async jwt({ token, user }) {
-      console.log('token', token);
-      console.log('user', user);
       if (user) {
         token.id = user.id;
         token.role = user.role;
@@ -62,7 +60,6 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
     async session({ session, token }) {
-      console.log('@', session, token);
       if (token) {
         session.user = {
           ...(session.user || {}),
